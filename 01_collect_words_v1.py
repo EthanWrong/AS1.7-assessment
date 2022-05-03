d8f8f07478ec59df4c0a3c668bbd543ba3f6e2b5
EthanWrong <98923515+EthanWrong@users.noreply.github.com>
2022-05-03 19:29:18 +1200

    Create 01_collect_words_v1.py

---
+++ 01_collect_words_v1.py
@@ -0,0 +1,17 @@
+import dictionary
+
+items = dictionary.numbers_dict
+# print(items)
+
+# word = list(items.items())[3][1]
+# this method converts an item in a dictionary to a tuple, that contains the
+# name of the item of said dictionary and the inner dictionary like so:
+# (1, {'english': ['one', '1'], 'maori': ['tahi']})
+
+# print(word)
+
+# if "wha" in word["maori"]:
+#     print(True)
+
+
+master_list = dictionary.numbers_dict
