a436bcd53da45993e9794d928655bf7eb7d8e213
EthanWrong <98923515+EthanWrong@users.noreply.github.com>
2022-05-05 12:35:24 +1200

    Update 01_collect_words_v4.py

--- 01_collect_words_v4.py
+++ 01_collect_words_v4.py
@@ -1,39 +1,78 @@
-"""Code taken from v1.
+"""Code restarted.
+Instead of a dictionary of dictionaries, I used a list of dictionaries. Code
+is not yet complete, but the majority of the core work is done. There is proof
+that the code will work when all functionality is added.
+
+Code is much more elegant and efficient than v1, v2, and v3
 """
-import library
+import library_v2
 
 
-# code taken from https://favtutor.com/blogs/merge-dictionaries-python,
-# takes two dictionaries as arguments and combines them
-def merge_two_dicts(dict_1, dict_2):
-    result = dict_1 | dict_2
+# choice interpreter that checks choice is valid, and does action accordingly
+def choice_selection(master):
+    collections = []
+    choice = input("Choice: ").lower()
+    while True:
+        if choice == "all":
+            print("clear master list and add all collections to it")
+
+        elif choice == "x":
+            if collections:
+                print("exit choice selection")
+                break
+            else:
+                print("<error>, you need to pick some words")
+
+        # checks choice is a collection name
+        elif choice in library_v2.collections:
+            if choice not in collections:  # checks not already chosen
+
+                if choice == "numbers":
+                    result = add_collection(library_v2.numbers_collection,
+                                            master)
+
+                    collections.append("numbers")
+
+                elif choice == "days":
+
+                    collections.append("days")
+
+                elif choice == "months":
+
+                    collections.append("months")
+
+                print(f"Collection {choice} added")
+
+            else:
+                print("<error>, you already have added that collection")
+
+        else:
+            print("<error>, collection does not exist")
+
+        # checks if all collections have been added to master list
+        if len(collections) == len(library_v2.collections):
+            print("All collections have been selected.")
+            break
+
+        choice = input("Choose more? ").lower()
+
+    print("Exited.")
+    print(collections)
     return result
 
 
-# choice checker that checks choice is valid.
-def choice_checker(choice):
-    if choice in (dictionaries):
-        # add di
+# adds a collection list to a master list
+def add_collection(collection, master):
+    result = collection + master
+    return result
 
 
+# main routine
 
+master_list = []  # will contain word-sets
 
+master_list = choice_selection(master_list)
+print(master_list)
 
-
-
-
-
-
-
-
-
-
-
-master_list = merge_two_dicts(library.numbers_dict, library.test_dict)
-print(f"master_list: {master_list}")
-
-# The following method converts an item in the master_list to a tuple. This
-# means the tuple is a word-set. This tuple contains two items: a string of
-# the name of the word-set, and a dictionary of translations.
-word_set1 = list(master_list.items())[0]
-print(f"word-set: {word_set1}")
+# test code for how to access a word-set
+# print(library_v2.numbers_collection[0])
