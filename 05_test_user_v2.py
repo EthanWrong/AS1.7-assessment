91074aa9a66d5a5f5bd96c93fd25a512bd9f7935
EthanWrong <98923515+EthanWrong@users.noreply.github.com>
2022-05-11 14:18:06 +1200

    Update 05_test_user_v2.py

--- 05_test_user_v2.py
+++ 05_test_user_v2.py
@@ -29,12 +29,6 @@ def remember_question(question, answers, user_answer):
             "user_answer": user_answer}  # what the user answered
 
 
-def check_for_dict_repeats_in_list(list, index):
-    for i in range(len(list)):
-        questions = []
-        question = list[i][index]
-        if question ==
-
 # main routine
 
 # these variables will be set with the generate_question method
